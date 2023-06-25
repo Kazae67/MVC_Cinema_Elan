f84741eef505457e44af6abb6dcb1affeb3b0175
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-25 22:20:25 +0200

    table personne ajouté à castingsController

--- controller/CastingsController.php
+++ controller/CastingsController.php
@@ -11,11 +11,12 @@ class CastingsController {
         $pdo = Connect::Connexion();
         $request = $pdo->query("
         
-            SELECT prenom, nom, role_name, titre_film, id_film, id_role, id_acteur, path_img_film, path_img_acteur, path_img_role
+            SELECT personne.prenom, personne.nom, role.role_name, film.titre_film, film.id_film, role.id_role, acteur.id_acteur, film.path_img_film, acteur.path_img_acteur, role.path_img_role
             FROM film
             INNER JOIN casting ON casting.film_id = film.id_film
             INNER JOIN role ON casting.role_id = role.id_role
             INNER JOIN acteur ON casting.acteur_id = acteur.id_acteur
+            INNER JOIN personne ON acteur.id_personne = personne.id_personne
         ");
 
         // Affiche la vue listCastings.php
