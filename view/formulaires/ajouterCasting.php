cfec97a471164c490ba6f89c9a626fd2b47ce116
Kazae67 <Kazae.zaeka@gmail.com>
2023-06-30 09:45:27 +0200

    formulaires partie ajouterCasting terminé

--- view/formulaires/ajouterCasting.php
+++ view/formulaires/ajouterCasting.php
@@ -3,41 +3,45 @@ ob_start();
 ?>
 
 <!-- AJOUTER CASTING -->
-<form action="index.php?action=ajouterCasting" method="post">
+<div class="container">
+    <div class="content">
+        <form action="index.php?action=ajouterCasting" method="post">
 
-    <!-- FILM -->
-    <div class="form-group">
-        <label for="film_id">Film</label>
-        <select name="film_id" id="film_id" class="form-control">
-        <?php while ($film = $requestFilm->fetch(PDO::FETCH_ASSOC)) : ?>
-            <option value="<?= $film['id_film'] ?>"><?= $film['titre_film'] ?></option>
-        <?php endwhile; ?>
-        </select>
-    </div>
-    
-    <!-- ACTEUR -->
-    <div class="form-group">
-        <label for="acteur_id">Acteur</label>
-        <select name="acteur_id" id="acteur_id" class="form-control">
-        <?php while ($acteur = $requestActeur->fetch(PDO::FETCH_ASSOC)) : ?>
-            <option value="<?= $acteur['id_acteur'] ?>"><?= $acteur['acteurNomComplet'] ?></option>
-        <?php endwhile; ?>
-        </select>
-    </div>
+            <!-- FILM -->
+            <div class="form-group">
+                <label for="film_id">Film</label>
+                <select name="film_id" id="film_id" class="form-control">
+                <?php while ($film = $requestFilm->fetch(PDO::FETCH_ASSOC)) : ?>
+                    <option value="<?= $film['id_film'] ?>"><?= $film['titre_film'] ?></option>
+                <?php endwhile; ?>
+                </select>
+            </div>
+            
+            <!-- ACTEUR -->
+            <div class="form-group">
+                <label for="acteur_id">Acteur</label>
+                <select name="acteur_id" id="acteur_id" class="form-control">
+                <?php while ($acteur = $requestActeur->fetch(PDO::FETCH_ASSOC)) : ?>
+                    <option value="<?= $acteur['id_acteur'] ?>"><?= $acteur['acteurNomComplet'] ?></option>
+                <?php endwhile; ?>
+                </select>
+            </div>
 
-    <!-- ROLE -->
-    <div class="form-group">
-        <label for="role_id">Rôle</label>
-        <select name="role_id" id="role_id" class="form-control">
-        <?php while ($role = $requestRole->fetch(PDO::FETCH_ASSOC)) : ?>
-            <option value="<?= $role['id_role'] ?>"><?= $role['role_name'] ?></option>
-        <?php endwhile; ?>
-        </select>
-    </div>
+            <!-- ROLE -->
+            <div class="form-group">
+                <label for="role_id">Rôle</label>
+                <select name="role_id" id="role_id" class="form-control">
+                <?php while ($role = $requestRole->fetch(PDO::FETCH_ASSOC)) : ?>
+                    <option value="<?= $role['id_role'] ?>"><?= $role['role_name'] ?></option>
+                <?php endwhile; ?>
+                </select>
+            </div>
 
-    <!-- BOUTON -->
-    <button type="submit" name="submit" value="Ajouter le casting" onclick="return notificationCasting()">Ajouter le casting</button>
-</form>
+            <!-- BOUTON -->
+            <input type="submit" name="submit" value="Ajouter le casting" onclick="return notificationCasting()" class="submit-btn">
+        </form>
+    </div>
+</div>
 
 <script src="public/js/notificationCasting.js"></script>
 <?php
